8a1f81abfda2c0f60d7694409b4cb5f39f49a7ea
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2024-12-27 12:29:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
+from database import LocalDatabase
 
 st.title("🎈 My new app hi")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
