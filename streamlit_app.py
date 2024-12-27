48d3935e26a754575de2f0780d51e5933ce604fb
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2024-12-27 12:34:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from database import LocalDatabase
 
-st.title("🎈 My new app hi")
-db = LocalDatabase()
-df = db.load_data()
-st.dataframe(df)
+st.title("🎈 My new app hiee")
+#db = LocalDatabase()
+#df = db.load_data()
+#st.dataframe(df)
