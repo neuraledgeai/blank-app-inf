624128f2b9d81c242b09ea0c159ebd66be37f6d0
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2024-12-28 17:36:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,11 @@ from display import Presentation
 
 # Set page title and layout
 st.set_page_config(
-    page_title="CPI-US",
+    page_title="CPI Machine",
     layout="wide",
 )
 
 st.title("Consumer Price Index in the US")
+st.subheader("Consumer Price Index in the US")
 present = Presentation()
 present.cpi()
