60d35e9553830e474fe985bcd718a27b4ac11ed8
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2024-12-29 14:36:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,6 @@ present.cpi()
 content = """
 <div style="text-align: justify; font-size: 12px;">
     This is a sample text that is justified from the left end to the right end with a smaller font size.
-
-    
     You can write your content here and it will be displayed in a justified manner.
 </div>
 """
