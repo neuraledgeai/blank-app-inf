db92d77053e208c86337299d8d8bccb622d001f3
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2024-12-29 12:03:45 +0530

    Update display.py

--- display.py
+++ display.py
@@ -33,23 +33,42 @@ class Presentation:
     # Calculate percent change from 2023 actual to predicted mean
     percent_change_cpi = ((mean_cpi - actual_cpi_2023) / actual_cpi_2023) * 100
     percent_change_ccpi = ((mean_ccpi - actual_ccpi_2023) / actual_ccpi_2023) * 100
+
+    # Calculate Purchasing Power for 2023 and 2033
+    pp_cpi_2023 = 100 / df.loc["2023-01-01", "CPI"]
+    pp_cpi_2033 = 100 / df.loc["2033-01-01", "CPI"]
+    
+    pp_ccpi_2023 = 100 / df.loc["2023-01-01", "CCPI"]
+    pp_ccpi_2033 = 100 / df.loc["2033-01-01", "CCPI"]
+
+    # Calculate Percent Change in Purchasing Power
+    pp_cpi_change = ((pp_cpi_2033 - pp_cpi_2023) / pp_cpi_2023) * 100
+    pp_ccpi_change = ((pp_ccpi_2033 - pp_ccpi_2023) / pp_ccpi_2023) * 100
     
     if option == "CPI: All Items in U.S. City Average":
       result = "fig_cpi"
       cpi = mean_cpi.round(3)
-      percent_change = percent_change_cpi.round(3)
+      cpi_percent_change = percent_change_cpi.round(3)
+      purchasing_power = pp_cpi_2033.round(3)
+      purchasing_power_percent_change = pp_cpi_change.round(3)
     elif option == "Percent Change from Year Ago: CPI: All Items in U.S. City Average":
       result = "fig_cpi_pct_chg"
       cpi = mean_cpi.round(3)
-      percent_change = percent_change_cpi.round(3)
+      cpi_percent_change = percent_change_cpi.round(3)
+      purchasing_power = pp_cpi_2033.round(3)
+      purchasing_power_percent_change = pp_cpi_change.round(3)
     elif option == "CPI: All Items Less Food and Energy in U.S. City Average":
       result = "fig_ccpi"
       cpi = mean_ccpi.round(3)
-      percent_change = percent_change_ccpi.round(3)
+      cpi_percent_change = percent_change_ccpi.round(3)
+      purchasing_power = pp_ccpi_2033.round(3)
+      purchasing_power_percent_change = pp_ccpi_change.round(3)
     elif option == "Percent Change from Year Ago: CPI: All Items Less Food and Energy in U.S. City Average":
       result = "fig_ccpi_pct_chg"
       cpi = mean_ccpi.round(3)
-      percent_change = percent_change_ccpi.round(3)
+      cpi_percent_change = percent_change_ccpi.round(3)
+      purchasing_power = pp_ccpi_2033.round(3)
+      purchasing_power_percent_change = pp_ccpi_change.round(3)
       
     fig = self.model.predict(result = result)
     
@@ -68,5 +87,5 @@ class Presentation:
     st.plotly_chart(fig)
     
     col1, col2 = st.columns(2)
-    col1.metric("CPI", cpi, percent_change)
-    col2.metric("10 Year Avg. CPI (2023-33)", "462.196", "51.688%")
+    col1.metric("10 Year Avg. CPI (2023-33)", cpi, cpi_percent_change)
+    col2.metric("Purchasing Power (2023-33)", purchasing_power, purchasing_power_percent_change)
