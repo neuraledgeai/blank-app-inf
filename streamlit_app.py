f1171bbc81910e78657c71675b89a7be899afa4e
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2025-03-07 12:38:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,3 @@
 import streamlit as st
-from display import Presentation
 
-# Set page title and layout
-st.set_page_config(
-    page_title="CPI Machines",
-    layout="wide",
-)
-
-present = Presentation()
-
-st.title("Consumer Price Index in the USA")
-present.cpi()
-present.notes()
+st.title(":blue-background[ECNOWLEDGE]")
