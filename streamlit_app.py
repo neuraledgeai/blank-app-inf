ca2d6fbf041680f7064659fee453df245e7c82f2
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2025-03-07 12:46:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title(":blue[ECNOWLEDGE] - The Journal of Economic Knowlege!")
+st.title(":blue[ECNOWLEDGE, Vol.3 JAN-DEC 2024]")
+st.title("The Journal of Economic Knowlege!")
 st.subheader("Produced and Maitained by Dept. of Economics, SN College Sivagiri, Varkala.")
