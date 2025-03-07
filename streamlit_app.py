bbc5ae648ac1faed409b53ec730a1c781ecdaf5a
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2025-03-07 12:49:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.markdown(
-    "<h1 style='text-align: center; color: blue;'>ECNOWLEDGE, Vol.3 JAN-DEC 2024</h1>", 
+    "<h1 style='text-align: center;'>ECNOWLEDGE, Vol.3 JAN-DEC 2024</h1>", 
     unsafe_allow_html=True
 )
 st.title("The Journal of Economic Knowlege!")
